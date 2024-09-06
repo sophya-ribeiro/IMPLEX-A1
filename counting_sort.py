8526626d880fbc5593202708a21196013e470f22
SorayaFerreira <soraya.ferreira@ufms.br>
2024-09-06 06:36:45 -0400

    Adiciona o algoritmo counting sort já testado.

--- counting_sort.py
+++ counting_sort.py
@@ -2,15 +2,24 @@
 import sys
 
 
-def counting_sort():
+def counting_sort(array: list, array_ordered: list, max_value: int):
     """
     Algoritmo de ordenação Counting Sort.
     Esta função ordena um vetor (array) de modo crescente.
     Recebe o VALORES.
     """
-    pass
+    aux_array = [0 for _ in range(max_value)]
 
+    for index in range(0, len(array)):
+        aux_array[array[index]] = aux_array[array[index]] + 1
 
+    for i in range(1, max_value):
+        aux_array[i] = aux_array[i] + aux_array[i -1]
+    
+    for j in range(len(array)-1, -1, -1):
+        array_ordered[aux_array[array[j]]] = array[j]
+        aux_array[array[j]] = aux_array[array[j]] - 1
+     
 
 def main():
     """
@@ -26,16 +35,14 @@ def main():
             #Caso de list comprehension
             numbers = [int(line.strip()) for line in lines]
             
-        counting_sort()
-
-        '''
-        for index in range(0, len(numbers)-1):
-            print(f"{numbers[index]} ", end='')
-        print(f"{numbers[len(numbers)-1]}")
-        '''
+        array_ordered = [0 for _ in range(len(numbers))]
+        counting_sort(numbers, array_ordered, max(numbers))
+        print(array_ordered)
+        
 
     except FileNotFoundError:
         print("Erro: Arquivo '{}' não encontrado.".format(file_name))
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
