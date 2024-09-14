617b5562095c414a784b34e0608fa6b9ae582ad7
SorayaFerreira <soraya.ferreira@ufms.br>
2024-09-14 15:20:09 -0400

    Implementa testes separados.

--- testar_elementares.py
+++ testar_elementares.py
@@ -1,9 +1,5 @@
 import bubble_sort
-import counting_sort
-import heap_sort
-import quick_sort
 import insertion_sort
-import merge_sort
 
 import random
 import math
@@ -37,15 +33,11 @@ def gerar_vetor_quase_ordenado(n: int) -> list:
     return numeros
 
 
-def gerar_grafico(tamanhos, tempos_bubble, tempos_insertion, tempos_merge, tempos_quick, tempos_heap, tempos_counting, escolha: int):
+def gerar_grafico(tamanhos, tempos_bubble, tempos_insertion, escolha: int):
     fig, ax = plt.subplots()
 
     ax.plot(tamanhos, tempos_bubble, label='Bubble', marker='o')
     ax.plot(tamanhos, tempos_insertion, label='Insertion', marker='o')
-    ax.plot(tamanhos, tempos_merge, label='Merge', marker='o')
-    ax.plot(tamanhos, tempos_quick, label='Quick', marker='o')
-    ax.plot(tamanhos, tempos_heap, label='Heap', marker='o')
-    ax.plot(tamanhos, tempos_counting, label='Counting', marker='o')
 
     xticks_interval = 500
     xticks = np.arange(min(tamanhos), max(tamanhos) + xticks_interval, xticks_interval)
@@ -55,14 +47,14 @@ def gerar_grafico(tamanhos, tempos_bubble, tempos_insertion, tempos_merge, tempo
     ax.set_ylabel('Tempo de Execução (segundos)')
 
     if escolha == 1 :
-        ax.set_title('[[RANDOM]]')
+        ax.set_title('Vetor Aleatório')
     elif escolha == 2:
-        ax.set_title('[[NEARLY SORTED]]')
+        ax.set_title('Vetor Quase Ordenado')
     elif escolha == 3:
-        ax.set_title('[[SORTED]]')
+        ax.set_title('Vetor Ordenado')
     else:
         # Se a escolha for 4
-        ax.set_title('[[REVERSE]]')
+        ax.set_title('Vetor Reverso')
     ax.legend()
     ax.grid(True)
 
@@ -72,7 +64,7 @@ def testar_vetor_aleatorio(fim: int, inc: int, stp: int):
     rpt = int(input("\tParâmetro rpt (número de repetições para média): "))
 
     print()
-    print(f"{'Tamanho':<10}{'Bubble':<10}{'Insertion':<10}{'Merge':<10}{'Heap':<10}{'Quick':<10}{'Counting':<10}")
+    print(f"{'Tamanho':<10}{'Bubble':<10}{'Insertion':<10}")
 
     # É o número de pontos de medição que o gráfico deverá apresentar
     n_pontos = ((fim - inc) // stp)+ 1
@@ -80,10 +72,6 @@ def testar_vetor_aleatorio(fim: int, inc: int, stp: int):
     # Aqui são as listas de testes, em cada posição haverá a média dos tempos de execução 
     tempos_bubble = [0.0] * n_pontos
     tempos_insertion = [0.0] * n_pontos
-    tempos_heap = [0.0] * n_pontos
-    tempos_quick = [0.0] * n_pontos
-    tempos_merge = [0.0] * n_pontos
-    tempos_counting = [0.0] * n_pontos
     tamanhos = []
 
     indice_tempos = 0 # iterador para percorrer o número de pontos de medição
@@ -105,10 +93,6 @@ def testar_vetor_aleatorio(fim: int, inc: int, stp: int):
             #cópias do vetor gerado aleatóriamente para passagem como parâmetro para cada algoritmo de ordenação
             copia_bubble = vetor.copy()
             copia_insertion = vetor.copy()
-            copia_heap = vetor.copy()
-            copia_merge = vetor.copy()
-            copia_quick = vetor.copy()
-            copia_counting = vetor.copy()
 
             # Medindo tempo do BUBBLE SORT
             start = time.time()
@@ -121,46 +105,16 @@ def testar_vetor_aleatorio(fim: int, inc: int, stp: int):
             insertion_sort.insertion_sort(copia_insertion)
             end = time.time()
             tempos_insertion[indice_tempos] += (end - start)
-
-            # Medindo tempos do HEAP SORT
-            start = time.time()
-            heap_sort.heap_sort(copia_heap)
-            end = time.time()
-            tempos_heap[indice_tempos] += (end - start)
-
-            # Medindo tempo do MERGE SORT
-            start = time.time()
-            merge_sort.merge_sort(copia_merge, 0, len(copia_merge) -1)
-            end = time.time()
-            tempos_merge[indice_tempos] += (end - start)
-
-            # Medindo tempo do QUICK SORT
-            start = time.time()
-            quick_sort.quick_sort(copia_quick, 0, len(copia_quick)-1)
-            end = time.time()
-            tempos_quick[indice_tempos] += (end - start)
-
-            # Medindo tempo do COUNTING SORT
-            start = time.time()
-            array_ordered = [0 for _ in range(len(copia_counting))]
-            max_value = max(copia_counting)
-            counting_sort.counting_sort(copia_counting, array_ordered, max_value)
-            end = time.time()
-            tempos_counting[indice_tempos] += (end - start)
         
         # Coloca a média na posição corresponde da lista de testes, conforme o tamanho do vetor utilizado.
         tempos_bubble[indice_tempos] /= rpt
         tempos_insertion[indice_tempos] /= rpt
-        tempos_heap[indice_tempos] /= rpt
-        tempos_quick[indice_tempos] /= rpt
-        tempos_merge[indice_tempos] /= rpt
-        tempos_counting[indice_tempos] /= rpt 
 
-        print(f"{n:<10}{tempos_bubble[indice_tempos]:<10.6f}{tempos_insertion[indice_tempos]:<10.6f}{tempos_merge[indice_tempos]:<10.6f}{tempos_heap[indice_tempos]:<10.6f}{tempos_quick[indice_tempos]:<10.6f}{tempos_counting[indice_tempos]:<10.6f}")
+        print(f"{n:<10}{tempos_bubble[indice_tempos]:<10.6f}{tempos_insertion[indice_tempos]:<10.6f}")
 
         indice_tempos += 1
 
-    gerar_grafico(tamanhos, tempos_bubble, tempos_insertion, tempos_merge, tempos_quick, tempos_heap, tempos_counting, 1)
+    gerar_grafico(tamanhos, tempos_bubble, tempos_insertion, 1)
     print()
 
 
@@ -171,7 +125,7 @@ def testar_caso_unico(fim: int, inc: int, stp: int, escolha: int):
     realizar uma média de tempos.
     """
     print()
-    print(f"{'Tamanho':<10}{'Bubble':<10}{'Insertion':<10}{'Merge':<10}{'Heap':<10}{'Quick':<10}{'Counting':<10}")
+    print(f"{'Tamanho':<10}{'Bubble':<10}{'Insertion':<10}")
 
     # É o número de pontos de medição que o gráfico deverá apresentar
     n_pontos = ((fim - inc) // stp)+ 1
@@ -179,10 +133,6 @@ def testar_caso_unico(fim: int, inc: int, stp: int, escolha: int):
     # Aqui são as listas de testes, em cada posição haverá o tempo de execução de um algoritmo 
     tempos_bubble = [0.0] * n_pontos
     tempos_insertion = [0.0] * n_pontos
-    tempos_heap = [0.0] * n_pontos
-    tempos_quick = [0.0] * n_pontos
-    tempos_merge = [0.0] * n_pontos
-    tempos_counting = [0.0] * n_pontos
     tamanhos = [] # guarda a lista de valores n que o vetor
 
     indice_tempos = 0 # iterador para percorrer o número de pontos de medição
@@ -211,7 +161,6 @@ def testar_caso_unico(fim: int, inc: int, stp: int, escolha: int):
         else: 
             #vetor = (gerar_numeros_aleatorios(n)).sort(reverse=True)
 
-            
             # Gera o vetor decrescente
             vetor = [value for value in range(n, 0, -1)]
             # if(indice_tempos == 0 or indice_tempos == 1):
@@ -220,10 +169,6 @@ def testar_caso_unico(fim: int, inc: int, stp: int, escolha: int):
         #cópias do vetor gerado aleatóriamente para passagem como parâmetro para cada algoritmo de ordenação
         copia_bubble = vetor.copy()
         copia_insertion = vetor.copy()
-        copia_heap = vetor.copy()
-        copia_merge = vetor.copy()
-        copia_quick = vetor.copy()
-        copia_counting = vetor.copy()
 
         # Medindo tempo do BUBBLE SORT
         start = time.time()
@@ -236,38 +181,12 @@ def testar_caso_unico(fim: int, inc: int, stp: int, escolha: int):
         insertion_sort.insertion_sort(copia_insertion)
         end = time.time()
         tempos_insertion[indice_tempos] += (end - start)
-
-        # Medindo tempos do HEAP SORT
-        start = time.time()
-        heap_sort.heap_sort(copia_heap)
-        end = time.time()
-        tempos_heap[indice_tempos] += (end - start)
-
-        # Medindo tempo do MERGE SORT
-        start = time.time()
-        merge_sort.merge_sort(copia_merge, 0, len(copia_merge) -1)
-        end = time.time()
-        tempos_merge[indice_tempos] += (end - start)
-
-        # Medindo tempo do QUICK SORT
-        start = time.time()
-        quick_sort.quick_sort(copia_quick, 0, len(copia_quick)-1)
-        end = time.time()
-        tempos_quick[indice_tempos] += (end - start)
-
-        # Medindo tempo do COUNTING SORT
-        start = time.time()
-        array_ordered = [0 for _ in range(len(copia_counting))]
-        max_value = max(copia_counting)
-        counting_sort.counting_sort(copia_counting, array_ordered, max_value)
-        end = time.time()
-        tempos_counting[indice_tempos] += (end - start)
         
-        print(f"{tempos_insertion[indice_tempos]:<10.6f}{tempos_merge[indice_tempos]:<10.6f}{tempos_heap[indice_tempos]:<10.6f}{tempos_quick[indice_tempos]:<10.6f}{tempos_counting[indice_tempos]:<10.6f}")
+        print(f"{n:<10}{tempos_bubble[indice_tempos]:<10.6f}{tempos_insertion[indice_tempos]:<10.6f}")
 
         indice_tempos += 1
 
-    gerar_grafico(tamanhos, tempos_bubble, tempos_insertion, tempos_merge, tempos_quick, tempos_heap, tempos_counting, escolha)
+    gerar_grafico(tamanhos, tempos_bubble, tempos_insertion, escolha)
     print()
 
 #----------- MAIN -----------#
--- testar_todos.py
+++ testar_todos.py
@@ -1,6 +1,8 @@
+import bubble_sort
 import counting_sort
 import heap_sort
 import quick_sort
+import insertion_sort
 import merge_sort
 
 import random
@@ -35,15 +37,25 @@ def gerar_vetor_quase_ordenado(n: int) -> list:
     return numeros
 
 
-def gerar_grafico(tamanhos, tempos_merge, tempos_quick, tempos_heap, tempos_counting, escolha: int):
+def gerar_grafico(tamanhos, tempos_bubble, tempos_insertion, tempos_merge, tempos_quick, tempos_heap, tempos_counting, escolha: int):
     fig, ax = plt.subplots()
 
+    ax.plot(tamanhos, tempos_bubble, label='Bubble', marker='o')
+    ax.plot(tamanhos, tempos_insertion, label='Insertion', marker='o')
     ax.plot(tamanhos, tempos_merge, label='Merge', marker='o')
     ax.plot(tamanhos, tempos_quick, label='Quick', marker='o')
     ax.plot(tamanhos, tempos_heap, label='Heap', marker='o')
     ax.plot(tamanhos, tempos_counting, label='Counting', marker='o')
 
+    # ax.plot(tamanhos, tempos_bubble, label='Bubble')
+    # ax.plot(tamanhos, tempos_insertion, label='Insertion')
+    # ax.plot(tamanhos, tempos_merge, label='Merge')
+    # ax.plot(tamanhos, tempos_quick, label='Quick')
+    # ax.plot(tamanhos, tempos_heap, label='Heap')
+    # ax.plot(tamanhos, tempos_counting, label='Counting')
+
     xticks_interval = 500
+    plt.xticks(fontsize=6)
     xticks = np.arange(min(tamanhos), max(tamanhos) + xticks_interval, xticks_interval)
     ax.set_xticks(xticks)
 
@@ -51,15 +63,14 @@ def gerar_grafico(tamanhos, tempos_merge, tempos_quick, tempos_heap, tempos_coun
     ax.set_ylabel('Tempo de Execução (segundos)')
 
     if escolha == 1 :
-        ax.set_title('Vetor Aleatório')
+        ax.set_title('[[RANDOM]]')
     elif escolha == 2:
-        ax.set_title('Vetor Quase Ordenado')
+        ax.set_title('[[NEARLY SORTED]]')
     elif escolha == 3:
-        ax.set_title('Vetor Ordenado')
+        ax.set_title('[[SORTED]]')
     else:
         # Se a escolha for 4
-        ax.set_title('Vetor Reverso')
-
+        ax.set_title('[[REVERSE]]')
     ax.legend()
     ax.grid(True)
 
@@ -69,12 +80,14 @@ def testar_vetor_aleatorio(fim: int, inc: int, stp: int):
     rpt = int(input("\tParâmetro rpt (número de repetições para média): "))
 
     print()
-    print(f"{'Tamanho':<10}{'Merge':<10}{'Heap':<10}{'Quick':<10}{'Counting':<10}")
+    print(f"{'Tamanho':<10}{'Bubble':<10}{'Insertion':<10}{'Merge':<10}{'Heap':<10}{'Quick':<10}{'Counting':<10}")
 
     # É o número de pontos de medição que o gráfico deverá apresentar
     n_pontos = ((fim - inc) // stp)+ 1
 
     # Aqui são as listas de testes, em cada posição haverá a média dos tempos de execução 
+    tempos_bubble = [0.0] * n_pontos
+    tempos_insertion = [0.0] * n_pontos
     tempos_heap = [0.0] * n_pontos
     tempos_quick = [0.0] * n_pontos
     tempos_merge = [0.0] * n_pontos
@@ -98,10 +111,24 @@ def testar_vetor_aleatorio(fim: int, inc: int, stp: int):
             vetor = gerar_numeros_aleatorios(n)
 
             #cópias do vetor gerado aleatóriamente para passagem como parâmetro para cada algoritmo de ordenação
+            copia_bubble = vetor.copy()
+            copia_insertion = vetor.copy()
             copia_heap = vetor.copy()
             copia_merge = vetor.copy()
             copia_quick = vetor.copy()
             copia_counting = vetor.copy()
+
+            # Medindo tempo do BUBBLE SORT
+            start = time.time()
+            bubble_sort.bubble_sort(copia_bubble, len(copia_bubble))
+            end = time.time()
+            tempos_bubble[indice_tempos] += (end - start)
+
+            # Medindo tempo do INSERTION SORT
+            start = time.time()
+            insertion_sort.insertion_sort(copia_insertion)
+            end = time.time()
+            tempos_insertion[indice_tempos] += (end - start)
 
             # Medindo tempos do HEAP SORT
             start = time.time()
@@ -130,16 +157,18 @@ def testar_vetor_aleatorio(fim: int, inc: int, stp: int):
             tempos_counting[indice_tempos] += (end - start)
         
         # Coloca a média na posição corresponde da lista de testes, conforme o tamanho do vetor utilizado.
+        tempos_bubble[indice_tempos] /= rpt
+        tempos_insertion[indice_tempos] /= rpt
         tempos_heap[indice_tempos] /= rpt
         tempos_quick[indice_tempos] /= rpt
         tempos_merge[indice_tempos] /= rpt
         tempos_counting[indice_tempos] /= rpt 
 
-        print(f"{n:<10}{tempos_merge[indice_tempos]:<10.6f}{tempos_heap[indice_tempos]:<10.6f}{tempos_quick[indice_tempos]:<10.6f}{tempos_counting[indice_tempos]:<10.6f}")
+        print(f"{n:<10}{tempos_bubble[indice_tempos]:<10.6f}{tempos_insertion[indice_tempos]:<10.6f}{tempos_merge[indice_tempos]:<10.6f}{tempos_heap[indice_tempos]:<10.6f}{tempos_quick[indice_tempos]:<10.6f}{tempos_counting[indice_tempos]:<10.6f}")
 
         indice_tempos += 1
 
-    gerar_grafico(tamanhos, tempos_merge, tempos_quick, tempos_heap, tempos_counting, 1)
+    gerar_grafico(tamanhos, tempos_bubble, tempos_insertion, tempos_merge, tempos_quick, tempos_heap, tempos_counting, 1)
     print()
 
 
@@ -150,17 +179,19 @@ def testar_caso_unico(fim: int, inc: int, stp: int, escolha: int):
     realizar uma média de tempos.
     """
     print()
-    print(f"{'Tamanho':<10}{'Merge':<10}{'Heap':<10}{'Quick':<10}{'Counting':<10}")
+    print(f"{'Tamanho':<10}{'Bubble':<10}{'Insertion':<10}{'Merge':<10}{'Heap':<10}{'Quick':<10}{'Counting':<10}")
 
     # É o número de pontos de medição que o gráfico deverá apresentar
     n_pontos = ((fim - inc) // stp)+ 1
 
     # Aqui são as listas de testes, em cada posição haverá o tempo de execução de um algoritmo 
+    tempos_bubble = [0.0] * n_pontos
+    tempos_insertion = [0.0] * n_pontos
     tempos_heap = [0.0] * n_pontos
     tempos_quick = [0.0] * n_pontos
     tempos_merge = [0.0] * n_pontos
     tempos_counting = [0.0] * n_pontos
-    tamanhos = [] # guarda a lista de valores n que o vetor
+    tamanhos = [] # guarda a lista de valores n que o vetor assume.
 
     indice_tempos = 0 # iterador para percorrer o número de pontos de medição
 
@@ -171,6 +202,7 @@ def testar_caso_unico(fim: int, inc: int, stp: int, escolha: int):
         """
 
         tamanhos.append(n)
+        
 
         if escolha == 2:
             # recebe um vetor com 10% de seus valores embaralhados entre si.
@@ -194,10 +226,24 @@ def testar_caso_unico(fim: int, inc: int, stp: int, escolha: int):
             #     print(vetor)
 
         #cópias do vetor gerado aleatóriamente para passagem como parâmetro para cada algoritmo de ordenação
+        copia_bubble = vetor.copy()
+        copia_insertion = vetor.copy()
         copia_heap = vetor.copy()
         copia_merge = vetor.copy()
         copia_quick = vetor.copy()
         copia_counting = vetor.copy()
+
+        # Medindo tempo do BUBBLE SORT
+        start = time.time()
+        bubble_sort.bubble_sort(copia_bubble, len(copia_bubble))
+        end = time.time()
+        tempos_bubble[indice_tempos] += (end - start)
+
+        # Medindo tempo do INSERTION SORT
+        start = time.time()
+        insertion_sort.insertion_sort(copia_insertion)
+        end = time.time()
+        tempos_insertion[indice_tempos] += (end - start)
 
         # Medindo tempos do HEAP SORT
         start = time.time()
@@ -225,11 +271,11 @@ def testar_caso_unico(fim: int, inc: int, stp: int, escolha: int):
         end = time.time()
         tempos_counting[indice_tempos] += (end - start)
         
-        print(f"{n:<10}{tempos_merge[indice_tempos]:<10.6f}{tempos_heap[indice_tempos]:<10.6f}{tempos_quick[indice_tempos]:<10.6f}{tempos_counting[indice_tempos]:<10.6f}")
+        print(f"{n:<10}{tempos_bubble[indice_tempos]:<10.6f}{tempos_insertion[indice_tempos]:<10.6f}{tempos_merge[indice_tempos]:<10.6f}{tempos_heap[indice_tempos]:<10.6f}{tempos_quick[indice_tempos]:<10.6f}{tempos_counting[indice_tempos]:<10.6f}")
 
         indice_tempos += 1
 
-    gerar_grafico(tamanhos, tempos_merge, tempos_quick, tempos_heap, tempos_counting, escolha)
+    gerar_grafico(tamanhos, tempos_bubble, tempos_insertion, tempos_merge, tempos_quick, tempos_heap, tempos_counting, escolha)
     print()
 
 #----------- MAIN -----------#
